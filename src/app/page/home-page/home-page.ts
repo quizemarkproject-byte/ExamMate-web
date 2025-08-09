1e8fd9351d73b66aca118d1ec9751f3d0174f19c
Kjeff24 <jeffreyarthur123@gmail.com>
2025-08-09 19:36:04 +0000

    Added home page

---
+++ src/app/page/home-page/home-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Footer } from '../../components/footer/footer';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [Footer],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.css'
+})
+export class HomePage {
+
+}
