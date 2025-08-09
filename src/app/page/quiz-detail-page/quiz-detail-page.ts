f7a445a0c21ee027872162649899b6cec421fca7
Kjeff24 <jeffreyarthur123@gmail.com>
2025-08-09 19:36:21 +0000

    Included quiz pages

---
+++ src/app/page/quiz-detail-page/quiz-detail-page.ts
@@ -0,0 +1,213 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { QuizService } from '../../services/quiz-service/quiz-service';
+import { Quiz, QuizResultResponse, QuizSubmission } from '../../models/quiz';
+import { FormsModule } from '@angular/forms';
+import { QuestionComponent } from '../../components/question-component/question-component';
+import { ActivatedRoute } from '@angular/router';
+import { TimerComponent } from '../../components/timer-component/timer-component';
+import { parseDurationToSeconds } from '../../utils/duration-utils';
+
+@Component({
+  selector: 'app-quiz-detail-page',
+  imports: [CommonModule, FormsModule, QuestionComponent, TimerComponent],
+  templateUrl: './quiz-detail-page.html',
+  styleUrl: './quiz-detail-page.css',
+})
+export class QuizDetailPage {
+  quizId: string = '';
+  quizData: Quiz | null = null;
+  quizSubmission: QuizSubmission = {
+    userId: 'user1',
+    quizSessionId: '',
+    answerSubmissions: [],
+  };
+  quizSubmissionResult: QuizResultResponse | null = null;
+  termsAccepted = false;
+  loading = false;
+  currentQuestionIndex = 0;
+  totalTimeLimitSeconds = 0;
+  remainingSeconds = 0;
+  private countdownInterval: any;
+
+  constructor(
+    private quizService: QuizService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.quizId = this.route.snapshot.paramMap.get('quizId')!;
+
+    const savedState = localStorage.getItem(`quiz-${this.quizId}`);
+    if (savedState) {
+      const parsed = JSON.parse(savedState);
+      this.quizData = parsed.quizData;
+      this.quizSubmission = parsed.quizSubmission;
+      this.currentQuestionIndex = parsed.currentQuestionIndex;
+      this.termsAccepted = parsed.termsAccepted;
+
+      if (this.quizData?.timeLimit) {
+        this.totalTimeLimitSeconds = parseDurationToSeconds(
+          this.quizData.timeLimit
+        );
+      }
+    }
+
+    if (this.termsAccepted) {
+      this.loadRemainingTimeFromServer();
+    }
+  }
+
+  loadRemainingTimeFromServer() {
+    this.loading = true;
+    this.quizService
+      .getRemainingTime(
+        this.quizId,
+        this.quizSubmission.userId,
+        this.quizSubmission.quizSessionId
+      )
+      .subscribe({
+        next: (time) => {
+          this.remainingSeconds = time.remainingSeconds;
+          this.totalTimeLimitSeconds = time.totalTimeLimitSeconds;
+          this.loading = false;
+          this.startCountdown();
+        },
+        error: (err) => {
+          console.error('Failed to load remaining time:', err);
+          this.loading = false;
+        },
+      });
+  }
+
+  loadQuiz() {
+    this.termsAccepted = true;
+    this.loading = true;
+
+    this.quizService
+      .startQuiz(this.quizId, this.quizSubmission.userId)
+      .subscribe({
+        next: (quiz: Quiz) => {
+          console.log('Quiz started:', quiz);
+          this.quizData = quiz;
+          this.quizSubmission.quizSessionId = quiz.timeRemaining?.quizSessionId || '';
+
+          if (quiz.timeLimit) {
+            this.totalTimeLimitSeconds = parseDurationToSeconds(quiz.timeLimit);
+          }
+
+          if (quiz.timeRemaining) {
+            this.remainingSeconds = quiz.timeRemaining.remainingSeconds;
+            this.totalTimeLimitSeconds =
+              quiz.timeRemaining.totalTimeLimitSeconds;
+            this.loading = false;
+            this.startCountdown();
+          }
+
+          this.saveState();
+        },
+        error: (err) => {
+          console.error(err);
+          this.loading = false;
+        },
+      });
+  }
+
+  onAnswerSelected(answer: string) {
+    const questionId = this.quizData!.questions![this.currentQuestionIndex].id;
+
+    let answerEntry = this.quizSubmission.answerSubmissions.find(
+      (a) => a.questionId === questionId
+    );
+
+    if (answerEntry) {
+      answerEntry.selectedAnswer = answer;
+    } else {
+      this.quizSubmission.answerSubmissions.push({
+        questionId,
+        selectedAnswer: answer,
+      });
+    }
+    this.saveState();
+  }
+
+  getSelectedAnswer(questionId: string): string | null {
+    return (
+      this.quizSubmission.answerSubmissions.find(
+        (a) => a.questionId === questionId
+      )?.selectedAnswer || null
+    );
+  }
+
+  hasAnswerForCurrentQuestion(): boolean {
+    const questionId =
+      this.quizData?.questions?.[this.currentQuestionIndex]?.id;
+    return !!this.quizSubmission.answerSubmissions.find(
+      (a) => a.questionId === questionId
+    );
+  }
+
+  nextQuestion() {
+    if (
+      this.quizData &&
+      (this.quizData.questions?.length ?? 0) > this.currentQuestionIndex + 1
+    ) {
+      this.currentQuestionIndex++;
+      this.saveState();
+    }
+  }
+
+  prevQuestion() {
+    if (this.currentQuestionIndex > 0) {
+      this.currentQuestionIndex--;
+      this.saveState();
+    }
+  }
+
+  saveState() {
+    localStorage.setItem(
+      `quiz-${this.quizId}`,
+      JSON.stringify({
+        quizData: this.quizData,
+        quizSubmission: this.quizSubmission,
+        currentQuestionIndex: this.currentQuestionIndex,
+        termsAccepted: this.termsAccepted,
+      })
+    );
+  }
+
+  private startCountdown() {
+    if (this.countdownInterval) {
+      clearInterval(this.countdownInterval);
+    }
+
+    this.countdownInterval = setInterval(() => {
+      if (this.remainingSeconds > 0) {
+        this.remainingSeconds--;
+      } else {
+        clearInterval(this.countdownInterval);
+        this.submitQuiz();
+      }
+    }, 1000);
+  }
+
+  submitQuiz() {
+    if (this.quizSubmission != null) {
+      this.quizService.submitQuiz(this.quizId, this.quizSubmission).subscribe({
+        next: (data: QuizResultResponse) => {
+          this.quizSubmissionResult = data;
+          localStorage.removeItem(`quiz-${this.quizId}`);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.countdownInterval) {
+      clearInterval(this.countdownInterval);
+    }
+  }
+}
---
+++ src/app/page/quiz-list-page/quiz-list-page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Quiz } from '../../models/quiz';
+import { QuizService } from '../../services/quiz-service/quiz-service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { DurationPipe } from '../../pipes/duration/duration-pipe';
+import { Footer } from '../../components/footer/footer';
+
+@Component({
+  selector: 'app-quiz-list-page',
+  imports: [CommonModule, RouterLink, DurationPipe, Footer],
+  templateUrl: './quiz-list-page.html',
+  styleUrl: './quiz-list-page.css',
+})
+export class QuizListPage {
+  quizData: Quiz[] | null = null;
+  loading = false;
+
+  constructor(private quizService: QuizService) {}
+
+  ngOnInit() {
+    this.quizService.getQuizzes().subscribe({
+      next: (data: Quiz[]) => {
+        this.quizData = data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+}
