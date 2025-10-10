512c2bd2c61e9c8e1d5bfa1511da6798c6fec011
Kjeff24 <jeffreyarthur123@gmail.com>
2025-10-10 18:23:32 +0000

    Added hamburger for side bar

--- src/app/components/side-bar/side-bar.ts
+++ src/app/components/side-bar/side-bar.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { TokenService } from '../../services/token-service/token-service';
+import { CommonModule } from '@angular/common';
 
 interface NavLink {
   label: string;
@@ -10,11 +11,14 @@ interface NavLink {
 
 @Component({
   selector: 'app-side-bar',
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './side-bar.html',
 })
 export class SideBar {
-  isLoggedIn:boolean = false;
+  isSidebarOpen = true;
+  isLoggedIn: boolean = false;
+  readonly MOBILE_BREAKPOINT = 1024;
+
   constructor(private tokenService: TokenService, private router: Router) {}
 
   navLinks: NavLink[] = [
@@ -25,6 +29,22 @@ export class SideBar {
 
   ngOnInit() {
     this.isLoggedIn = this.tokenService.isLoggedIn();
+
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    const width = window.innerWidth;
+    this.isSidebarOpen = width >= this.MOBILE_BREAKPOINT;
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
   }
 
   logout() {
