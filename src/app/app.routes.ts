1a79abbe67ce8efdf31c19599a941aa74b87fd09
Kjeff24 <jeffreyarthur123@gmail.com>
2025-10-15 09:36:14 +0000

    Restrctured quiz result

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
-import { LoginPage } from './pages/login-page/login-page';
 import { HomePage } from './pages/home-page/home-page';
 import { QuizDetailPage } from './pages/quiz-page/quiz-detail-page/quiz-detail-page';
 import { QuizListPage } from './pages/quiz-page/quiz-list-page/quiz-list-page';
 import { QuizPage } from './pages/quiz-page/quiz-page/quiz-page';
-import { QuizResultDetail } from './pages/result-page/result-detail/result-detail';
-import { QuizResultList } from './pages/result-page/result-list/result-list';
-import { QuizResultPage } from './pages/result-page/result-page/result-page';
-import { reverseAuthGuard } from './guards/reverse-auth/reverse-auth-guard';
+import { QuizResultDetail } from './pages/quiz-page/result-detail/result-detail';
+import { QuizResultList } from './pages/quiz-page/result-list/result-list';
 
 export const routes: Routes = [
   {
@@ -22,7 +19,7 @@ export const routes: Routes = [
   },
   {
     path: 'result',
-    component: QuizResultPage,
+    component: QuizPage,
     children: [
       {
         path: '',
